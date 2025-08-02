a857388180b50415508c5e02807928abe825e8ea
Aukasha <aukashakhan30@gmail.com>
2025-08-02 09:35:14 +0500

    updated code

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/Sales/',
   plugins: [react()],
   server: {
     host: '127.0.0.1',
     port: 5173,
     open: true
   },
-  base: '/Sales/',
+
 })
