bf63e652bf3f996df5f139b15f58a31142e86700
Ameer Hamza <ameer.hamza1811@gmail.com>
2025-08-14 03:20:41 +0500

    fix: correct image URL path in ImageUrl function

--- src/utils/Functions.ts
+++ src/utils/Functions.ts
@@ -2,6 +2,6 @@ const { hostname } = window.location;
 
 export const ImageUrl = (image: string) => {
   if (hostname.includes("github"))
-    return `./Sales/images/${image}`;
+    return `/Sales/images/${image}`;
   else return `/Sales/images/${image}`;
 };
