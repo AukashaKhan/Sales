f0e00149577e40149764071052299b28951f3051
Aukasha <aukashakhan30@gmail.com>
2025-08-14 02:58:38 +0500

    updated code

--- src/utils/Functions.ts
+++ src/utils/Functions.ts
@@ -2,6 +2,6 @@ const { hostname } = window.location;
 
 export const ImageUrl = (image: string) => {
   if (hostname.includes("github"))
-    return `./images/${image}`;
+    return `./Sales/images/${image}`;
   else return `./images/${image}`;
 };
